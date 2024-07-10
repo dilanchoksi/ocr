d70b973aaea25f6e60db40233d51d5c813d85b0b
Dilan N Choksi <dilan.choksi@wakefern.com>
2024-07-10 14:58:01 -0400

    ocr commit

--- ocr.py
+++ ocr.py
@@ -38,3 +38,27 @@ def predict(self, test):
 
     results = y2.T.tolist()[0]
     return results.index(max(results))
+
+    def save(self):
+        if not self._use_file:
+            return
+
+        json_neural_network = {
+            "theta1":[np_mat.tolist()[0] for np_mat in self.theta1],
+            "theta2":[np_mat.tolist()[0] for np_mat in self.theta2],
+            "b1":self.input_layer_bias[0].tolist()[0],
+            "b2":self.hidden_layer_bias[0].tolist()[0]
+        };
+        with open(OCRNeuralNetwork.NN_FILE_PATH,'w') as nnFile:
+            json.dump(json_neural_network, nnFile)
+
+    def _load(self):
+        if not self._use_file:
+            return
+
+        with open(OCRNeuralNetwork.NN_FILE_PATH) as nnFile:
+            nn = json.load(nnFile)
+        self.theta1 = [np.array(li) for li in nn['theta1']]
+        self.theta2 = [np.array(li) for li in nn['theta2']]
+        self.input_layer_bias = [np.array(nn['b1'][0])]
+        self.hidden_layer_bias = [np.array(nn['b2'][0])]
